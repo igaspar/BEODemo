2d29978061871ec696689b60a1793d6659c186c6
novasolutionsystems <adelgado@novasolutionsystems.com>
2017-06-14 09:23:08 -0500

    cambia el endPoint

--- server.js
+++ server.js
@@ -133,11 +133,11 @@ app.post(`/v1/login`, (req, res) => {
 app.post(`/v1/accesoCliente`, (req, res) => {
   
  
-if(req.body.banderaAutentificacion=="")
+if(req.body.banderaAcceso=="")
    res.json(consultaContrato );
   
 else {
-	if(req.body.banderaAutentificacion=="1")
+	if(req.body.banderaAcceso=="1")
       res.json(autentificacion);
     else
       res.json(errorServicio);
